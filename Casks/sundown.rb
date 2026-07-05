c41c713a213e423cf109ba4b4c59b5f38cbc5783
andreicovaciu <andrei_covaciu98@yahoo.com>
2026-07-05 15:34:07 +0300

    Update Sundown cask to 0.3.0

--- Casks/sundown.rb
+++ Casks/sundown.rb
@@ -1,6 +1,6 @@
 cask "sundown" do
-  version "0.2.0"
-  sha256 "62a27a1f5b6ddc24ed7a0a6c8cf7db40018ca845fc3bf9206c5c18b5189950eb"
+  version "0.3.0"
+  sha256 "786468a3d90a598e4d23a6019afa1ba7438647106bbbdb7cebbe1c6434b24368"
 
   url "https://github.com/andreicovaciu/sundown/releases/download/v#{version}/Sundown-#{version}.zip"
   name "Sundown"
